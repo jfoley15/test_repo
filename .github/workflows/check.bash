532b78d523bafb71bdde2d055f92d9563419a7e4
John Foley <jfoley@pokerstarsint.com>
2023-05-11 16:14:28 +0100

    New method

--- .github/workflows/check.bash
+++ .github/workflows/check.bash
@@ -2,7 +2,7 @@
 
 set -e
 
-repo_count=$(find . -type d -mindepth 2 -maxdepth 2 2>/dev/null | grep -c -v '^.$')
+repo_count=$(find . -type d -maxdepth 1 2>/dev/null | grep -c -v '^.$')
 readme_count=$(find . -type f -name 'README.md' -maxdepth 2 2>/dev/null | wc -l)
 echo readme_count is $readme_count
 echo repo_count is $repo_count
@@ -10,5 +10,5 @@ status="success"
 if (( repo_count != readme_count )); then
   status="failure"
 fi
-
+echo status is $status
 echo "status=$status" >> "$GITHUB_OUTPUT"
